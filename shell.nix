40e06728a66e03d25343a668cd44ef2a652b2da2
Mattia Gheda <ghedamat@gmail.com>
2019-09-14 21:49:23 -0400

    init

---
+++ shell.nix
@@ -0,0 +1,15 @@
+let
+  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+  ruststable = (nixpkgs.latest.rustChannels.stable.rust.override { extensions = [ "rust-src" "rls-preview" "rust-analysis" "rustfmt-preview" ];});
+in
+  with nixpkgs;
+  stdenv.mkDerivation {
+    name = "rust";
+    buildInputs = [ openssl nasm rustup ruststable cmake zlib gnumake gcc readline openssl libxml2 curl];
+
+    shellHook = ''
+        export OPENSSL_DIR="${openssl.dev}"
+        export OPENSSL_LIB_DIR="${openssl.out}/lib"
+    '';
+  }
---
+++ src/main.rs
@@ -0,0 +1,78 @@
+use structopt::StructOpt;
+use clap_verbosity_flag::Verbosity;
+
+//use failure::ResultExt;
+use exitfailure::ExitFailure;
+
+use log::{info, warn};
+
+use std::str::FromStr;
+
+// Handling command parsing
+// taken from https://github.com/Peternator7/strum/blob/master/strum/src/lib.rs#L55
+#[derive(Debug)]
+enum ParseError {
+  VariantNotFound,
+}
+
+impl std::fmt::Display for ParseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+        // We could use our macro here, but this way we don't take a dependency on the
+        // macros crate.
+        match self {
+            &ParseError::VariantNotFound => write!(f, "Use --help for info"),
+        }
+    }
+}
+
+
+// available commands
+#[derive(Debug)]
+enum Command {
+  Check,
+  Clone,
+  Status,
+  //Help
+}
+
+impl FromStr for Command {
+    type Err = ParseError;
+    fn from_str(day: &str) -> Result<Self, Self::Err> {
+        match day {
+            "check" => Ok(Command::Check),
+            "clone" => Ok(Command::Clone),
+            "status" => Ok(Command::Status),
+            _ => Err(ParseError::VariantNotFound),
+        }
+    }
+}
+
+// CLI definition
+#[derive(Debug, StructOpt)]
+struct Cli {
+
+  #[structopt(flatten)]
+  verbose: Verbosity,
+  #[structopt(flatten)]
+  log: clap_log_flag::Log,
+
+  /// Available commands: check, clone or status
+  command: Command,
+}
+
+fn main() -> Result<(), ExitFailure> {
+  let args = Cli::from_args();
+  args.log.log_all(args.verbose.log_level())?;
+
+  warn!("LogLevel Warn");
+  info!("LogLevel Info");
+
+  match args.command {
+    //Command::Help => print_help(),
+    Command::Status => (),
+    Command::Check => (),
+    Command::Clone => (),
+  };
+
+  Ok(())
+}

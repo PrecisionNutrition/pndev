082017cd23cb7980fb3420ba6a337182f1599037
Mattia Gheda <ghedamat@Mattias-MacBook-Pro.local>
2019-09-29 11:53:45 -0400

    bump 0.1.4

--- shell.nix
+++ shell.nix
@@ -1,15 +1,22 @@
+with (import <nixpkgs> {});
 let
   moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
   nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
   ruststable = (nixpkgs.latest.rustChannels.stable.rust.override { extensions = [ "rust-src" "rls-preview" "rust-analysis" "rustfmt-preview" ];});
+  basePackages = [ openssl nasm rustup ruststable cmake zlib gnumake gcc readline openssl libxml2 curl];
+
+  inputs = if system == "x86_64-darwin" then
+              basePackages ++ [darwin.apple_sdk.frameworks.CoreServices]
+           else
+              basePackages;
 in
   with nixpkgs;
   stdenv.mkDerivation {
     name = "rust";
-    buildInputs = [ openssl nasm rustup ruststable cmake zlib gnumake gcc readline openssl libxml2 curl];
+    buildInputs = inputs;
 
     shellHook = ''
-        export OPENSSL_DIR="${openssl.dev}"
-        export OPENSSL_LIB_DIR="${openssl.out}/lib"
+        export OPENSSL_DIR="${nixpkgs.openssl.dev}"
+        export OPENSSL_LIB_DIR="${nixpkgs.openssl.out}/lib"
     '';
   }

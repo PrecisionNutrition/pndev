c56e40d5ee5170ef3a712e58c41f54dfa6f6a493
Mattia Gheda <ghedamat@gmail.com>
2019-10-03 14:22:05 -0400

    no token needed for a public repo

--- src/update.rs
+++ src/update.rs
@@ -7,13 +7,10 @@ use std::os::unix::fs::PermissionsExt;
 // see https://github.com/jaemk/self_update#usage
 /// Performs a self update of pndev itself
 pub fn run() -> Result<(), Error> {
-    let token = std::env::var("DOWNLOAD_AUTH_TOKEN")
-        .expect("DOWNLOAD_AUTH_TOKEN needs to the best in your environment");
     let status = self_update::backends::github::Update::configure()
         .repo_owner("PrecisionNutrition")
         .repo_name("pndev")
         .bin_name("pndev-linux-amd64")
-        .auth_token(&token)
         .show_download_progress(true)
         .current_version(self_update::cargo_crate_version!())
         .build()?

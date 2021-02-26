32eba5f4f86df6987e8181d5ee9d123eae93b503
Benjamin Borowski <ben@weareokidoki.com>
2021-02-25 17:31:09 -0800

    chore: update nix shell
    https://nixos.wiki/wiki/Rust

--- shell.nix
+++ shell.nix
@@ -1,22 +1,23 @@
 with (import <nixpkgs> {});
 let
-  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
-  nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
-  ruststable = (nixpkgs.latest.rustChannels.stable.rust.override { extensions = [ "rust-src" "rls-preview" "rust-analysis" "rustfmt-preview" ];});
-  basePackages = [ openssl nasm rustup ruststable cmake zlib gnumake gcc readline openssl libxml2 curl];
+  # Pinned nixpkgs, deterministic. Last updated: 2/12/21.
+  pkgs = import (fetchTarball("https://github.com/NixOS/nixpkgs/archive/a58a0b5098f0c2a389ee70eb69422a052982d990.tar.gz")) {};
+
+  basePackages = [
+    pkgs.cargo
+    pkgs.rustc
+    pkgs.rustfmt
+    pkgs.rustup
+  ];
 
   inputs = if system == "x86_64-darwin" then
               basePackages ++ [darwin.apple_sdk.frameworks.CoreServices]
            else
               basePackages;
-in
-  with nixpkgs;
-  stdenv.mkDerivation {
-    name = "rust";
-    buildInputs = inputs;
 
-    shellHook = ''
-        export OPENSSL_DIR="${nixpkgs.openssl.dev}"
-        export OPENSSL_LIB_DIR="${nixpkgs.openssl.out}/lib"
-    '';
-  }
+in pkgs.mkShell {
+  buildInputs = inputs;
+
+  # See https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570/3?u=samuela.
+  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+}

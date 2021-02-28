02b9fe1c117f2b9d182f2cd4b15c2974f6e6d98f
Mattia Gheda <ghedamat@gmail.com>
2021-02-28 01:27:43 +0000

    use niv to manage version of the nix channel
    we do this in our other nixfiles and it works pretty well
    I pushed directly to your branch @typeoneerror, hope that's ok

--- shell.nix
+++ shell.nix
@@ -1,23 +1,18 @@
-with (import <nixpkgs> {});
+# to update the nix-channel run `nix-shell -p niv --run 'niv update'`
+{ sources ? import ./nix/sources.nix }:
+with (import sources.nixpkgs { });
 let
-  # Pinned nixpkgs, deterministic. Last updated: 2/12/21.
-  pkgs = import (fetchTarball("https://github.com/NixOS/nixpkgs/archive/a58a0b5098f0c2a389ee70eb69422a052982d990.tar.gz")) {};
-
-  basePackages = [
-    pkgs.cargo
-    pkgs.rustc
-    pkgs.rustfmt
-    pkgs.rustup
-  ];
+  basePackages =
+    [ cargo gcc openssl pkg-config readline rustc rustfmt rustup zlib ];
 
   inputs = if system == "x86_64-darwin" then
-              basePackages ++ [darwin.apple_sdk.frameworks.CoreServices]
-           else
-              basePackages;
+    basePackages ++ [ darwin.apple_sdk.frameworks.CoreServices ]
+  else
+    basePackages;
 
 in pkgs.mkShell {
   buildInputs = inputs;
 
   # See https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570/3?u=samuela.
-  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+  RUST_SRC_PATH = "${rust.packages.stable.rustPlatform.rustLibSrc}";
 }
